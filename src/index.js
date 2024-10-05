433c02e7ab8c67e8ccd7afaf5489f1fb17279aad
RodrigoDornelles <rodrigo.dornelles.poa@gmail.com>
2024-10-05 13:56:19 -0300

    feat: start integration with gly the engine (#1)
    * change: start use gridsystem
    * change: add gly game engine

--- src/index.js
+++ src/index.js
@@ -1,129 +1,95 @@
+import wasmFile from 'wasmoon/dist/glue.wasm';
+import { LuaFactory, LuaMultiReturn } from 'wasmoon'
 import { editor, KeyMod, KeyCode } from 'monaco-editor'
+import gly from '@gamely/core-native-html5'
+import gly_engine from '@gamely/gly-engine/dist/main.lua'
 import defaultScript from './default.lua'
 
-const monacoEditor = editor.create(document.getElementById('editor'), {
-    language: 'lua',
-    theme: 'vs-dark',
-    automaticLayout: true,
-    fontLigatures: true,
-    fontFamily: 'Cascadia Code'
-});
+document.addEventListener('DOMContentLoaded', async () => {
+    const elementEditor = document.querySelector('#editor')
+    const monacoEditor = editor.create(elementEditor, {
+        language: 'lua',
+        theme: 'vs-dark',
+        automaticLayout: true,
+        fontLigatures: true,
+        fontFamily: 'Cascadia Code'
+    });
 
-(async () => {
-    if (window.pasteFetch) {
-        try {
-            const res = await window.pasteFetch
-            if (res.ok) {
-                monacoEditor.setValue(await res.text())
-            }
-        } catch (err) {
-            console.error(err)
-        }
-    }
+    monacoEditor.setValue(defaultScript)
+    
+    const factory = new LuaFactory(wasmFile)
+    const lua = await factory.createEngine()
+    await lua.doString(gly_engine)
 
-    if (!monacoEditor.getValue()) {
-        monacoEditor.setValue(defaultScript)
-    }
+    gly.global.set('native_callback_init', lua.global.get('native_callback_init'))
+    gly.global.set('native_callback_loop', lua.global.get('native_callback_loop'))
+    gly.global.set('native_callback_draw', lua.global.get('native_callback_draw'))
+    gly.global.set('native_callback_resize', lua.global.get('native_callback_resize'))
+    gly.global.set('native_callback_keyboard', lua.global.get('native_callback_keyboard'))
+    lua.global.set('native_draw_start', gly.global.get('native_draw_start'))
+    lua.global.set('native_draw_flush', gly.global.get('native_draw_flush'))
+    lua.global.set('native_draw_clear', gly.global.get('native_draw_clear'))
+    lua.global.set('native_draw_color', gly.global.get('native_draw_color'))
+    lua.global.set('native_draw_font', gly.global.get('native_draw_font'))
+    lua.global.set('native_draw_rect', gly.global.get('native_draw_rect'))
+    lua.global.set('native_draw_line', gly.global.get('native_draw_line'))
+    lua.global.set('native_draw_image', gly.global.get('native_draw_image'))
+    lua.global.set('native_dict_http', gly.global.get('native_dict_http'))
+    lua.global.set('native_dict_json', gly.global.get('native_dict_json'))
+    lua.global.set('native_dict_poly', gly.global.get('native_dict_poly'))
+    lua.global.set('native_draw_text', (x, y, text) => {
+        const native_draw_text = gly.global.get('native_draw_text')
+        return LuaMultiReturn.from(native_draw_text(x, y, text))
+    })
 
-    const outputEl = document.getElementById('output')
+    gly.error('stop, canvas, console')
+    gly.init('#gameCanvas')
 
-    let runner, isRunning = false
-    const createRunner = () => {
-        if (runner && isRunning) {
-            runner.terminate()
-            runner = undefined
-        }
-
-        if (!runner) {
-            runner = new Worker(new URL('./runner.js', import.meta.url),
-                { name: 'Lua runner' })
-        }
-
-        isRunning = true
-
-        runner.onmessage = ({ data: { type, data } }) => {
-            document.getElementById('running')?.remove()
-
-            if (type === 'finished') {
-                outputEl.style.color = '#FFFFFF'
-                isRunning = false
-            } else if (type === 'log') {
-                const logEl = document.createElement('pre')
-                logEl.innerText = data
-                outputEl.appendChild(logEl)
-            } else if (type === 'error') {
-                const logEl = document.createElement('span')
-                logEl.innerText = data
-                logEl.style.color = '#FF0000'
-                outputEl.appendChild(logEl)
-            } else if (type === 'clear') {
-                outputEl.innerHTML = ''
-            }
-        }
-
-        return runner
-    }
-
-    const runLua = async () => {
-        outputEl.style.color = '#888888'
-        outputEl.innerHTML = '<span id="running">Running...</span>'
-
-        createRunner().postMessage({
-            type: 'execute',
-            data: monacoEditor.getValue()
-        })
-    }
-
-    await runLua()
+    gly.load(defaultScript)
 
     monacoEditor.addAction({
-        id: 'execute-action',
-        label: 'Execute code',
+        id: 'run-game',
+        label: 'running game',
         keybindings: [KeyMod.CtrlCmd | KeyCode.KeyE],
-        run: runLua
-    })
-
-    monacoEditor.addAction({
-        id: 'stop-action',
-        label: 'Stop running code',
-        keybindings: [KeyMod.CtrlCmd | KeyCode.KeyM],
         run: () => {
-            if (runner && isRunning) {
-                runner.terminate()
-                runner = undefined
-                outputEl.style.color = '#FFFFFF'
-                isRunning = false
-            }
+            gly.load(monacoEditor.getValue())
         }
     })
 
-    monacoEditor.addAction({
-        id: 'save-action',
-        label: 'Save code',
-        keybindings: [KeyMod.CtrlCmd | KeyCode.KeyS],
-        run: async (editor) => {
-            try {
-                document.getElementById('current-action').innerText = 'Saving...'
+    const keys = [
+        [403, 'red'],
+        [404, 'green'],
+        [405, 'yellow'],
+        [406, 'blue'],
+        ['KeyZ', 'red'],
+        ['KeyX', 'green'],
+        ['KeyC', 'yellow'],
+        ['KeyV', 'blue'],
+        ['Enter', 'enter'],
+        ['ArrowUp', 'up'],
+        ['ArrowDown', 'down'],
+        ['ArrowLeft', 'left'],
+        ['ArrowRight', 'right'],
+    ];
 
-                const res = await fetch('https://api.ceifa.dev/documents', {
-                    method: 'POST',
-                    body: monacoEditor.getValue()
-                })
+    function updateSize() {
+        gly.resize()
+    }
 
-                if (res.ok) {
-                    const body = await res.json()
-                    history.pushState(body, document.title, `?paste=${body.key}`)
-                }
-
-                // copy to clipboard
-                await navigator.clipboard.writeText(location.href)
-
-                document.getElementById('current-action').innerText = 'Copied to clipboard'
-            } finally {
-                setTimeout(() => {
-                    document.getElementById('current-action').innerText = ''
-                }, 3000)
-            }
+    function updateKey(ev) {
+        const key = keys.find(key => key[0] == ev.code)
+        if (key) {
+            gly.input(key[1], Number(ev.type === 'keydown'))
         }
-    })
-})()
+    }
+
+    function updateLoop() {
+        window.requestAnimationFrame(updateLoop);
+        gly.update((new Date()).getTime())
+    }
+
+    window.addEventListener("resize", updateSize);
+    window.addEventListener('keydown', updateKey)
+    window.addEventListener('keyup', updateKey)
+    window.requestAnimationFrame(updateLoop);
+})
