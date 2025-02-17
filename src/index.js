a593f10997cfff70e4933a5a943b664fe97ea992
RodrigoDornelles <rodrigo@dornelles.me>
2025-02-17 10:08:48 -0300

    fix: export button

--- src/index.js
+++ src/index.js
@@ -4,8 +4,12 @@ import { editor, KeyMod, KeyCode } from 'monaco-editor'
 import gly from '@gamely/core-native-html5'
 import gly_engine from '@gamely/gly-engine/dist/main.lua'
 import defaultScript from './default.lua'
+import devices from '../devices.json'
 import applyLayout from './ui.js'
+import downloadGame from './export.js'
+import compulate from './compulate.js'
 
+compulate(devices)
 applyLayout('layout-default');
 let monacoTimeout;
 
@@ -104,83 +108,27 @@ document.addEventListener('DOMContentLoaded', async () => {
     });
     
     elBtnExport.addEventListener('click', async () => {
+        const select = document.querySelector('#preset-resolutions');
+        const option = select.querySelector(`option[value="${select.value}"]`)
+        const device_name = option.textContent.split(' ')[0]
+        const device = devices.find((device) => device.short.toLowerCase() == device_name.toLowerCase())
+
+        if (!device) {
+            return console.error(`device not found: ${device_name}`)
+        }
+
+        if (!device.template) {
+            return console.error(`${device.name} build is comming soon!`)
+        }
+
         if (elBtnExport.disabled) {
-            return;
+            return console.warn(`building already in progress!`);
         }
-    
+        
+        console.log(`starting build ${device.name}`)
         elBtnExport.disabled = true;
-    
-        const templatePrefix = '--GLYSTART ';
-        const content = monacoEditor.getValue();
-    
-        try {
-            const response = await fetch('https://get.gamely.com.br/game.gba');
-            if (!response.ok) {
-                throw new Error(`Erro ao buscar o arquivo de origem: ${response.statusText}`);
-            }
-    
-            const srcBuffer = await response.arrayBuffer();
-            const srcContent = new Uint8Array(srcBuffer);
-            const templateBytes = new TextEncoder().encode(templatePrefix);
-            let start = -1;
-    
-            for (let i = 0; i <= srcContent.length - templateBytes.length; i++) {
-                if (srcContent.subarray(i, i + templateBytes.length).every((byte, index) => byte === templateBytes[index])) {
-                    start = i;
-                    break;
-                }
-            }
-    
-            if (start === -1) {
-                throw new Error('Template não encontrado!');
-            }
-    
-            const sizeStart = start + templateBytes.length;
-            let sizeEnd = sizeStart;
-            while (sizeEnd < srcContent.length && srcContent[sizeEnd] >= 48 && srcContent[sizeEnd] <= 57) { // ASCII de '0'-'9'
-                sizeEnd++;
-            }
-            const size = parseInt(new TextDecoder().decode(srcContent.subarray(sizeStart, sizeEnd)), 10);
-    
-            if (isNaN(size)) {
-                throw new Error('Tamanho do template inválido!');
-            }
-    
-            const final = start + size;
-            const templateSize = final - start;
-    
-            if (templateSize < content.length) {
-                throw new Error(`Tamanho máximo permitido: ${templateSize} bytes.`);
-            }
-    
-            const paddingSize = templateSize - content.length;
-            const padding = new Uint8Array(paddingSize).fill(10);
-            const encodedContent = new TextEncoder().encode(content);
-            const outputContent = new Uint8Array(srcContent.length - (final - start) + encodedContent.length + paddingSize);
-    
-            outputContent.set(srcContent.subarray(0, start), 0);
-            outputContent.set(encodedContent, start);
-            outputContent.set(padding, start + encodedContent.length);
-            outputContent.set(srcContent.subarray(final), start + encodedContent.length + paddingSize);
-    
-            console.log('Final size:', outputContent.length);
-    
-            const blob = new Blob([outputContent], { type: 'application/octet-stream' });
-            const url = URL.createObjectURL(blob);
-    
-            const a = document.createElement('a');
-            a.href = url;
-            a.download = 'game.gba';
-            document.body.appendChild(a);
-            a.click();
-    
-            URL.revokeObjectURL(url);
-            document.body.removeChild(a);
-        } catch (error) {
-            console.error('Export error:', error.message);
-        } finally {
-            elBtnExport.disabled = false;
-        }
+        await downloadGame(monacoEditor.getValue(), device.template)
+        elBtnExport.disabled = false;
     });
 
     const keys = [
